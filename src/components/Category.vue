71d4b313d309d2688c253d1daccb358eec45502e
AbiAbdurrahman <abi_abdurrahman@hotmail.com>
2018-07-28 00:23:41 +0700

    belom ngapa2in nyehehe

---
+++ src/components/Category.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="category-page">
+    <div v-if="categoryChosen===true" class="category-options">
+      <div class="header">
+  			<h1>Kategori</h1>
+        <img v-bind:src="'src/assets/icon/baseline_close_24_px.svg'" />
+  		</div>
+      <div class="category-list">
+        <div class="category">
+          <b>Sports</b>
+
+        </div>
+      </div>
+    </div>
+    <div v-else class="category-results">
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        categoryChosen: true
+      }
+    }
+  }
+</script>
+
+<style>
+  .header {
+    text-align: center;
+    padding: 17px 24px;
+    height: 64px;
+    /* display: flex; */
+    /* align-items: baseline; */
+    border-bottom: 1px solid #dddddd;
+  }
+  .header h1 {
+    font-size: 30px;
+    font-weight: 900;
+    float: left;
+  }
+  .header img {
+    float: right;
+    margin-top: 10px;
+  }
+  .category {
+    padding: 18px 14px 18px 14px;
+    border-bottom: 1px solid #dddddd;
+  }
+</style>
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,11 @@
 import MainPage from './components/MainPage.vue';
 import DetailPage from './components/DetailPage.vue';
 import DetailMerchantPage from './components/DetailMerchantPage.vue';
+import Category from './components/Category.vue';
+
 export default [
   {path: '/', component: MainPage},
   {path: '/detail', component: DetailPage},
   {path: '/detail-merchant', component: DetailMerchantPage},
+  {path: '/category', component: Category}
 ]
