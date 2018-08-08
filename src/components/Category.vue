9c93e6aa7fd4dde474abb7494a6e4984b788a061
AbiAbdurrahman <abi_abdurrahman@hotmail.com>
2018-08-08 08:12:46 +0700

    ini bagian aq mah apa atuh

--- src/components/Category.vue
+++ src/components/Category.vue
@@ -5,14 +5,33 @@
   			<h1>Kategori</h1>
         <img v-bind:src="'src/assets/icon/baseline_close_24_px.svg'" />
   		</div>
-      <div class="category-list">
-        <div class="category">
-          <b>Sports</b>
-
+      <!-- <div class="category-list">
+        <div class="category" v-on:click="subCategoryChosen=!subCategoryChosen">
+          <b>Fashion</b>
         </div>
+        <div class="sub-category-list" v-if="subCategoryChosen===true">
+          <div class="sub-category">
+            <p>Atasan</p>
+          </div>
+          <div class="sub-category">
+            <p>Baju</p>
+          </div>
+        </div>
+      </div> -->
+      <div class="category-list" v-for="category in categories">
+        <div class="category" v-on:click="category.isActive=!category.isActive">
+          <b>{{ category.name }}</b>
+          <div class="sub-category-list" v-if="category.isActive===true">
+            <div class="sub-category" v-for="subCategory in category.subCategories" v-on:click.prevent.stop="subCategory.isActive=!subCategory.isActive" v-bind:class="{ 'hovered' : subCategory.isActive }">
+              <p>{{ subCategory.name }}</p>
+            </div>
+          </div>
+        </div>
+
       </div>
     </div>
     <div v-else class="category-results">
+
     </div>
   </div>
 </template>
@@ -21,7 +40,98 @@
   export default {
     data() {
       return {
-        categoryChosen: true
+        categoryChosen: true,
+        subCategoryChosen: false,
+        categories: [
+          {
+            name: 'Fashion',
+            isActive: false,
+            subCategories: [
+              {
+                name: 'Bawahan',
+                isActive: false
+              },
+              {
+                name: 'Atasan',
+                isActive: false
+              },
+              {
+                name: 'Sepatu',
+                isActive: false
+              },
+              {
+                name: 'Aksesories',
+                isActive: false
+              },
+              {
+                name: 'Jam Tangan',
+                isActive: false
+              },
+              {
+                name: 'Tas',
+                isActive: false
+              }
+            ]
+          },
+          {
+            name: 'Kendaraan',
+            isActive: false,
+            subCategories: [
+              {
+                name: 'Mobil',
+                isActive: false
+              },
+              {
+                name: 'Motor',
+                isActive: false
+              }
+            ]
+          },
+          {
+            name: 'Handmade',
+            isActive: false,
+            subCategories: [
+              {
+                name: 'Kerajinan Kayu',
+                isActive: false
+              },
+              {
+                name: 'Kerajinan Plastik',
+                isActive: false
+              },
+              {
+                name: 'Import',
+                isActive: false
+              }
+            ]
+          },
+          {
+            name: 'Buku',
+            isActive: false,
+            subCategories: [
+              {
+                name: 'Fiksi',
+                isActive: false
+              },
+              {
+                name: 'Non-fiksi',
+                isActive: false
+              },
+              {
+                name: 'Agama',
+                isActive: false
+              },
+              {
+                name: 'Majalah',
+                isActive: false
+              },
+              {
+                name: 'Buku Pelajaran',
+                isActive: false
+              }
+            ]
+          }
+        ]
       }
     }
   }
@@ -48,5 +158,29 @@
   .category {
     padding: 18px 14px 18px 14px;
     border-bottom: 1px solid #dddddd;
+  }
+  .sub-category-list {
+    /* padding: 5px 10px 5px 10px; */
+  }
+  .sub-category-list .sub-category {
+    /* padding: 6px 8px ; */
+    padding-left: 6px;
+    padding-right: 6px;
+    background-color: #ececec;
+    border-radius: 12px;
+    /* margin-left: 5px; */
+    margin-right: 10px;
+    margin-top: 10px;
+    width: max-content;
+    height: 26px;
+    clear: none;
+    display: inline-block;
+  }
+  .hovered {
+    color: #00d54d;
+    background-color: #2e00d54d;
+  }
+  .sub-category-list .sub-category p {
+    display: inline-block;
   }
 </style>
