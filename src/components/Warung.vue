0294ecfe436de25539509a3a28e5e3d6c56107d6
Abdurrahan Saleh <abi_abdurrahman@hotmail.com>
2018-09-04 19:03:20 +0700

    warung diedit edit

--- src/components/Warung.vue
+++ src/components/Warung.vue
@@ -23,7 +23,7 @@
                       </div>
                     </td>
                     <td class="table-top" style="width:12px;"></td>
-                    <td class="table-top" style="max-height:28px;">{{warungs[selected-1].alamat}}</td>
+                    <td class="table-top" style="max-height:28px;line-height:16px;">{{warungs[selected-1].alamat}}</td>
                   </tr>
                 </table>
               </div>
@@ -61,21 +61,21 @@
             <tr style="margin-top:32px;">
               <td class="table-top"><img v-bind:src="'src/assets/icon/'+ml.gambarMakanan" style="width:76px;height:76px;"/></td>
               <td class="table-top" style="padding-left:12px;">
-                <div style="font-size:14px;color:#222222;">
+                <div style="font-size:14px;color:#222222;line-height:16px;">
                   {{ml.namaMakanan}}
                 </div>
-                <div style="font-size:8px;color:#888888;">
+                <div style="font-size:8px;color:#888888;line-height:16px;">
                   {{ml.keteranganMakanan}}
                 </div>
-                <div style="margin-top:12px;font-size:14px;color:#222222;">
+                <div style="margin-top:12px;font-size:14px;color:#222222;line-height:16px;">
                   Rp {{ml.hargaMakanan.toLocaleString('id-ID')}}
                 </div>
               </td>
               <td class="table-top" style="padding-left:28px;">
                 <center>
-                  <img @click="ml.kuantitasMakanan+=1;quantity+=1;totalHarga+=ml.hargaMakanan;" v-bind:src="'src/assets/icon/group_3_1.svg'" style="width:24px;height:24px;"/>
-                  <h3 v-if="ml.kuantitasMakanan>0" style="font-size:14px;color:#222222;margin-top:7px;">{{ml.kuantitasMakanan}}</h3>
-                  <img @click="ml.kuantitasMakanan-=1;quantity-=1;totalHarga-=ml.hargaMakanan;" v-if="ml.kuantitasMakanan>0" v-bind:src="'src/assets/icon/group_4_1.svg'" style="width:24px;height:24px;"/>
+                  <img @click="ml.kuantitasMakanan+=1;quantity+=1;totalHarga+=ml.hargaMakanan;" v-bind:src="'src/assets/icon/group_3_1.svg'" style="width:24px;height:24px;margin-bottom:5px;"/>
+                  <h3 v-if="ml.kuantitasMakanan>0" style="font-size:14px;color:#222222;margin-top:0px;margin-bottom:0px;">{{ml.kuantitasMakanan}}</h3>
+                  <img @click="ml.kuantitasMakanan-=1;quantity-=1;totalHarga-=ml.hargaMakanan;" v-if="ml.kuantitasMakanan>0" v-bind:src="'src/assets/icon/group_4_1.svg'" style="width:24px;height:24px;margin-top:5px;"/>
                 </center>
               </td>
             </tr>
@@ -88,11 +88,11 @@
     <div v-if="quantity>0" class="all-count">
       <table>
         <tr>
-          <td style="font-size:14px;font-weight:bold;width:50%;">
+          <td style="font-size:14px;font-weight:bold;width:50%;line-height:16px;">
             <div>{{quantity}} items</div>
             <div style="margin-top:4px;">Rp {{totalHarga.toLocaleString('id-ID')}}</div>
           </td>
-          <td style="font-size:16px;font-weight:bold;width:50%;">
+          <td style="font-size:16px;font-weight:bold;width:50%;line-height:16px;">
             Lanjutkan
           </td>
         </tr>
@@ -183,6 +183,7 @@ export default {
   .title-header{
     font-size: 20px;
     font-weight: bold;
+    line-height: 18px;
     color: #000000;
   }
   .note-header{
